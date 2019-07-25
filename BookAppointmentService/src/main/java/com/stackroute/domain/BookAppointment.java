7b71fa57d3058e5dd30e778cd0df0b6cb8a936bd
Harshiiitha <harshithareddy829@gmail.com>
2019-07-25 14:31:54 +0530

    first commit

---
+++ BookAppointmentService/src/main/java/com/stackroute/domain/BookAppointment.java
@@ -0,0 +1,27 @@
+package com.stackroute.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookAppointment {
+
+    @Id
+    private String Id;
+    private Doctor doctor;
+    private Patient patient;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date appointmentDate;
+    private String appointmentId;
+    private String slot;
+
+}
